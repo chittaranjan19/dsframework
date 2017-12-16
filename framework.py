11f4c039fb02c9b0ff8d43d054f25af51bc4330b
Vishalkks <vishalkks@gmail.com>
2017-12-16 22:37:44 +0530

    initial design

--- framework.py
+++ framework.py
@@ -1,1 +1,33 @@
-from policies.model.decisionTrees import *
+from policies.model.model import *
+from policies.normalize.normalize import *
+from policies.test.testing import *
+from policies.preprocess.preprocess import *
+
+
+class Framework:
+    def __init__(self, data, preprocesspobj=PreProcess(), normalizepobj=Normalize(), modelpobj=Model(), testpobj=Testing()):
+        self.preProcesPObj = preprocesspobj
+        self.normalizePObj = normalizepobj
+        self.modelPObj = modelpobj
+        self.testPObj = testpobj
+        self.data = data
+
+    def setPreProcecssObj(self,preprocessPObj):
+        self.preProcesPObj = preprocessPObj
+
+    def setNormalizePObj(self,normalizePObj):
+        self.normalizePObj = normalizePObj
+
+    def setModelPObj(self, modelPObj):
+        self.modelPObj = modelPObj
+
+    def setTestingPObj(self,testPObj):
+        self.testPObj = testPObj
+
+
+    def processData(self):
+        #self.data includes both training and test data, including test data, parameters a little hazy, should decide on the design.
+        data = self.preProcesPObj.preProcess(self.data)
+        data = self.normalizePObj.normalize(data)
+        res = self.modelPObj.classify(data)
+        return self.testPObj.test(data, res)
