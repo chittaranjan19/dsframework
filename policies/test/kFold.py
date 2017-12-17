4ba54b17eb38f26fdcafee3bb70908085a54eb32
Vishalkks <vishalkks@gmail.com>
2017-12-18 04:17:01 +0530

    last commit for the night

--- policies/test/kFold.py
+++ policies/test/kFold.py
@@ -3,15 +3,15 @@ import copy
 from random import shuffle
 
 class KFold(Testing):
-    def __init__(self, K=10):
+    def __init__(self):
         Testing.__init__(self)
-        self.K = K
+        self.K = 10
 
     def test(self, frameworkObj):
         data = copy.deepcopy(frameworkObj.data)
         shuffle(data)
         percAccuracy = 0
-        modeledObj = frameworkObj.modelPolicy.build(data)
+        modeledObj = frameworkObj.modelPolicy.build(data, True)
         for train,test in self.genKFolds(data):
             modeledObj = frameworkObj.modelPolicy.build(train,True)
             numCorrect = 0
@@ -19,9 +19,9 @@ class KFold(Testing):
                 output = modeledObj.classify(instance[:-1])
                 if(output == instance[-1]):
                     numCorrect +=1
-            percAccuracy += (numCorrect/len(test))*100
+            percAccuracy += (float(numCorrect)/len(test))*100
 
-        return modeledObj, percAccuracy/self.K
+        return modeledObj, float(percAccuracy)/self.K
 
     def genKFolds(self, data):
         for k in xrange(self.K):
